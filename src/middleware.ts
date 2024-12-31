07cb999dbd13e621cb4b1b072b155ab3850b53eb
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-12-31 06:07:47 +0000

    middleware for cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,29 @@
 import { match } from "assert";
 import { NextResponse } from "next/server";
 
+const allowedOrigins = process.env.NODE_ENV === 'production' 
+? ['https://www.yoursite.com', 'https://yoursite.com']
+: ['http://localhost:3000', 'https://www.google.com']
+
 export function middleware(request: Request) {
 
-    const regex = new RegExp('/api/*')
+    // const regex = new RegExp('/api/*')
 
-    if (regex.test(request.url)) {
+    // if (regex.test(request.url)) {
         
+    // }
+
+    const origin = request.headers.get('origin')
+    console.log(origin)
+
+    if (origin && !allowedOrigins.includes(origin)) {
+        return new NextResponse(null, {
+            status: 400,
+            statusText: 'Bad request',
+            headers: {
+                'Content-Type': 'text/plain',
+            }
+        })
     }
 
     console.log('middleware')
@@ -14,8 +31,7 @@ export function middleware(request: Request) {
     console.log(request.method)
     console.log(request.url)
 
-    const origin = request.headers.get('origin')
-    console.log(origin)
+    
 
     return NextResponse.next()
 }
